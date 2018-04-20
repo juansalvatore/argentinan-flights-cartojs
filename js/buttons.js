647ba511dca73e559e3c4bc9409b5e0a2c66b322
Juan Salvatore <juansalvatore@live.com.ar>
2018-04-20 16:16:51 -0300

    Add corrections and new design styles

--- js/buttons.js
+++ js/buttons.js
@@ -2,49 +2,79 @@ $(document).ready(function() {
   const internacional = document.getElementById('internacional')
   const local = document.getElementById('local')
   const all = document.getElementById('all')
-  const ezeiza = document.getElementById('ezeiza')
-  const cordoba = document.getElementById('cordoba')
-  const mendoza = document.getElementById('mendoza')
+  // const ezeiza = document.getElementById('ezeiza')
+  // const cordoba = document.getElementById('cordoba')
+  // const mendoza = document.getElementById('mendoza')
 
   internacional.addEventListener('click', d => {
     internacional.classList.add('button-active')
     local.classList.remove('button-active')
     all.classList.remove('button-active')
+    document.getElementById('footer-text').innerHTML =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    document.getElementById('footer-number').innerHTML = '80.000'
+    document
+      .getElementById('footer-number')
+      .classList.add('number-color-internacionales')
+    document
+      .getElementById('footer-number')
+      .classList.remove('number-color-locales')
+    document.getElementById('footer-rutas').innerHTML = 'Rutas internacionales'
   })
 
   local.addEventListener('click', d => {
     local.classList.add('button-active')
     internacional.classList.remove('button-active')
     all.classList.remove('button-active')
+    document.getElementById('footer-text').innerHTML =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.'
+    document.getElementById('footer-number').innerHTML = '29.000'
+    document
+      .getElementById('footer-number')
+      .classList.remove('number-color-internacionales')
+    document
+      .getElementById('footer-number')
+      .classList.add('number-color-locales')
+    document.getElementById('footer-rutas').innerHTML = 'Rutas de cabotaje'
   })
 
   all.addEventListener('click', d => {
     all.classList.add('button-active')
     internacional.classList.remove('button-active')
     local.classList.remove('button-active')
+    document.getElementById('footer-text').innerHTML =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.'
+    document.getElementById('footer-number').innerHTML = '109.000'
+    document.getElementById('footer-rutas').innerHTML = 'Todas las rutas'
+    document
+      .getElementById('footer-number')
+      .classList.remove('number-color-internacionales')
+    document
+      .getElementById('footer-number')
+      .classList.remove('number-color-locales')
   })
 
-  ezeiza.addEventListener('click', d => {
-    ezeiza.classList.add('button-active')
-    cordoba.classList.remove('button-active')
-    mendoza.classList.remove('button-active')
-    document.getElementById('iframe').innerHTML =
-      '<iframe scrolling="no" width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/421bb584-7839-402d-9fad-ac5f3fafe42a/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
-  })
+  // ezeiza.addEventListener('click', d => {
+  //   ezeiza.classList.add('button-active')
+  //   cordoba.classList.remove('button-active')
+  //   mendoza.classList.remove('button-active')
+  //   document.getElementById('iframe').innerHTML =
+  //     '<iframe scrolling="no" width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/421bb584-7839-402d-9fad-ac5f3fafe42a/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
+  // })
 
-  cordoba.addEventListener('click', d => {
-    ezeiza.classList.remove('button-active')
-    cordoba.classList.add('button-active')
-    mendoza.classList.remove('button-active')
-    document.getElementById('iframe').innerHTML =
-      '<iframe width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/3e3629a8-8bcd-4547-8155-e1451df469a5/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
-  })
+  // cordoba.addEventListener('click', d => {
+  //   ezeiza.classList.remove('button-active')
+  //   cordoba.classList.add('button-active')
+  //   mendoza.classList.remove('button-active')
+  //   document.getElementById('iframe').innerHTML =
+  //     '<iframe width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/3e3629a8-8bcd-4547-8155-e1451df469a5/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
+  // })
 
-  mendoza.addEventListener('click', d => {
-    ezeiza.classList.remove('button-active')
-    cordoba.classList.remove('button-active')
-    mendoza.classList.add('button-active')
-    document.getElementById('iframe').innerHTML =
-      '<iframe width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/38dfff99-a460-4679-a193-85e6e6707def/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
-  })
+  // mendoza.addEventListener('click', d => {
+  //   ezeiza.classList.remove('button-active')
+  //   cordoba.classList.remove('button-active')
+  //   mendoza.classList.add('button-active')
+  //   document.getElementById('iframe').innerHTML =
+  //     '<iframe width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/38dfff99-a460-4679-a193-85e6e6707def/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
+  // })
 })
--- js/scroll.js
+++ js/scroll.js
@@ -4,48 +4,79 @@ let page = 0
 
 disableScroll()
 document.getElementById('arrow-up').classList.add('hidden-button')
+document.getElementById('footer-info').classList.add('hide-footer')
+document
+  .getElementById('scroll-buttons-container')
+  .classList.add('center-buttons')
 
 down.addEventListener('click', d => {
   if (page === 0) {
     document.getElementById('content').style.top = '-100vh'
     document.getElementById('arrow-up').classList.remove('hidden-button')
+    document
+      .getElementById('scroll-buttons-container')
+      .classList.remove('center-buttons')
+    document.getElementById('scroll-buttons-container').classList.add('up')
+    document.getElementById('footer-info').classList.remove('hide-footer')
     page++
     console.log(page)
   } else if (page === 1) {
     document.getElementById('content').style.top = '-200vh'
-    document.getElementById('iframe').innerHTML =
-      '<iframe scrolling="no" width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/421bb584-7839-402d-9fad-ac5f3fafe42a/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
-    document.getElementById('scroll-buttons-container').classList.add('up')
-    page++
-    console.log(page)
-  } else if (page === 2) {
-    document.getElementById('content').style.top = '-300vh'
+    // document.getElementById('iframe').innerHTML =
+    //   '<iframe scrolling="no" width="100%" height="100%" frameborder="0" src="https://datosgobar.carto.com/u/modernizacion/builder/421bb584-7839-402d-9fad-ac5f3fafe42a/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>'
+    // document.getElementById('scroll-buttons-container').classList.add('up')
     document.getElementById('scroll-buttons-container').classList.remove('up')
-    enableScroll()
+    document.getElementById('footer-info').classList.add('hide-footer')
+    setTimeout(() => {
+      document.getElementById('footer-info').style.display = 'none'
+    }, 300)
     page++
+    document.getElementById('arrow-down').classList.add('hidden-button')
+    enableScroll()
     console.log(page)
   }
+  // } else if (page === 2) {
+  //   document.getElementById('content').style.top = '-300vh'
+  //   // document.getElementById('scroll-buttons-container').classList.remove('up')
+  //   enableScroll()
+  //   page++
+  //   console.log(page)
+  // }
 })
 
 up.addEventListener('click', d => {
   if (page === 1) {
     document.getElementById('content').style.top = '0'
     document.getElementById('arrow-up').classList.add('hidden-button')
+    document
+      .getElementById('scroll-buttons-container')
+      .classList.add('center-buttons')
+    document.getElementById('scroll-buttons-container').classList.remove('up')
+    document.getElementById('footer-info').classList.add('hide-footer')
     page--
     console.log(page)
   } else if (page === 2) {
     document.getElementById('content').style.top = '-100vh'
     disableScroll()
     document.getElementById('scroll-buttons-container').classList.remove('up')
-    page--
-    console.log(page)
-  } else if (page === 3) {
-    document.getElementById('content').style.top = '-200vh'
-    disableScroll()
+    document.getElementById('arrow-down').classList.remove('hidden-button')
     document.getElementById('scroll-buttons-container').classList.add('up')
+
+    document.getElementById('footer-info').style.display = 'block'
+    setTimeout(() => {
+      document.getElementById('footer-info').classList.remove('hide-footer')
+    }, 100)
+
     page--
     console.log(page)
   }
+  // else if (page === 3) {
+  //   document.getElementById('content').style.top = '-200vh'
+  //   disableScroll()
+  //   document.getElementById('scroll-buttons-container').classList.add('up')
+  //   page--
+  //   console.log(page)
+  // }
 })
 
 var keys = { 37: 1, 38: 1, 39: 1, 40: 1 }
