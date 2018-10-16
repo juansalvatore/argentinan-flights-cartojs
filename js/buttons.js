0ea00502235b826594efc08b15b5e9bfbf172050
Juan Salvatore <juansalvatore@live.com.ar>
2018-10-16 12:42:53 -0300

    Refactor buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -2,44 +2,63 @@ $(document).ready(function() {
   const internacional = document.getElementById('internacional')
   const cabotaje = document.getElementById('cabotaje')
   const todos = document.getElementById('todos')
+  const txtNumber = document.getElementById('footer-number')
+  const txtRutas = document.getElementById('footer-rutas')
+  const txt = document.getElementById('footer-text')
 
-  internacional.addEventListener('click', d => {
-    internacional.classList.add('button-active')
-    cabotaje.classList.remove('button-active')
+  const content = [
+    {
+      route: todos,
+      number: '206.963',
+      coloredText: 'Vuelos totales entre sep17-sep18.',
+      text: '',
+    },
+    {
+      route: internacional,
+      number: '117.463',
+      coloredText: 'Vuelos internacionales entre sep17-sep18.',
+      text: 'Un 10% más de vuelos que en el período anterior.',
+    },
+    {
+      route: cabotaje,
+      number: '235.145',
+      coloredText: 'Vuelos de cabotaje entre sep17-sep18.',
+      text: 'Un 6% más de vuelos que en el período anterior.',
+    },
+  ]
+
+  content.forEach(({ route, number, coloredText, text }) => {
+    route.addEventListener('click', () => {
+      setButtons(route)
+      // Text
+      txtNumber.innerHTML = number
+      txtRutas.innerHTML = coloredText
+      txt.innerHTML = text
+      // Color
+      setNumberColor(route)
+    })
+  })
+
+  // Set active buttons on click
+  setButtons = button => {
+    // Reset buttons
     todos.classList.remove('button-active')
-    document.getElementById('footer-text').innerHTML = 'Un 10% más de vuelos que en el período anterior.'
-    document.getElementById('footer-number').innerHTML = '117.463'
-    document.getElementById('footer-number').classList.add('number-color-internacionales')
-    document.getElementById('footer-number').classList.remove('number-color-locales')
-    document.getElementById('footer-rutas').innerHTML = 'Vuelos internacionales entre sep17-sep18.'
-    document.getElementById('footer-rutas').classList.add('number-color-internacionales')
-    document.getElementById('footer-rutas').classList.remove('number-color-locales')
-  })
-
-  cabotaje.addEventListener('click', d => {
-    cabotaje.classList.add('button-active')
-    internacional.classList.remove('button-active')
-    todos.classList.remove('button-active')
-    document.getElementById('footer-text').innerHTML = 'Un 6% más de vuelos que en el período anterior.'
-    document.getElementById('footer-number').innerHTML = '235.145'
-    document.getElementById('footer-number').classList.remove('number-color-internacionales')
-    document.getElementById('footer-number').classList.add('number-color-locales')
-    document.getElementById('footer-rutas').innerHTML = 'Vuelos de cabotaje entre sep17-sep18.'
-
-    document.getElementById('footer-rutas').classList.remove('number-color-internacionales')
-    document.getElementById('footer-rutas').classList.add('number-color-locales')
-  })
-
-  todos.addEventListener('click', d => {
-    todos.classList.add('button-active')
     internacional.classList.remove('button-active')
     cabotaje.classList.remove('button-active')
-    document.getElementById('footer-text').innerHTML = 'Un 10% más de vuelos que en el período anterior.'
-    document.getElementById('footer-number').innerHTML = '206.963'
-    document.getElementById('footer-rutas').innerHTML = 'Vuelos de cabotaje entre ago-17 y ago-18.'
-    document.getElementById('footer-number').classList.remove('number-color-internacionales')
-    document.getElementById('footer-number').classList.remove('number-color-locales')
-    document.getElementById('footer-rutas').classList.remove('number-color-internacionales')
-    document.getElementById('footer-rutas').classList.remove('number-color-locales')
-  })
+    // Add active class
+    button.classList.add('button-active')
+  }
+  // Set footer colors on click
+  setNumberColor = route => {
+    txtNumber.classList.remove('number-color-internacionales', 'number-color-locales')
+    txtRutas.classList.remove('number-color-internacionales', 'number-color-locales')
+
+    if (route === internacional) {
+      txtNumber.classList.add('number-color-internacionales')
+      txtRutas.classList.add('number-color-internacionales')
+    } else if (route === cabotaje) {
+      txtNumber.classList.add('number-color-locales')
+      txtRutas.classList.add('number-color-locales')
+    }
+  }
 })
